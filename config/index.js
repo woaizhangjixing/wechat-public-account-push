c4106111ba138e7828953ff48568d619e3aa67b3
woaizhangjixing <113014362+woaizhangjixing@users.noreply.github.com>
2022-09-07 17:11:20 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -16,24 +16,25 @@ export const config = {
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
    * [
-   *  {
+   *  {oQbhN5oNECIpZtjD2ZFxHTmyySM0
    *    // 想要发送的人的名字
    *    name: "老婆0",
    *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-   *    id: "ohxOO6VskYv0Kuf-ywIZVRMpuXhk",
+   *    id: "oQbhN5oNECIpZtjD2ZFxHTmyySM0",
    *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "zjVglIuCXo4CsraXWEio1iP7sZJ7IQEES9lDsgc4yOU",
+   *    useTemplateId: "9zH50yAkhRs77b60z mU Tsoezt9oBpWktX
+MMFhEkalQ",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
    *    openUrl: "https://wangxinleo.cn"
    *   }, 
    * ]
    */
   USERS: [
-    {oQbhN5oNECIpZtjD2ZFxHTmyySM0
+    {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oQbhN5oNECIpZtjD2ZFxHTmyySM0",
+      id: "",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "9zH50yAkhRs77b60z mU Tsoezt9oBpWktX
 MMFhEkalQ",
